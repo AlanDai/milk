8e2a9a10bb7fc2678771a8811d663f540fd90710
alanxudai@gmail.com <alanxudai@gmail.com>
2021-03-30 18:43:54 -0700

    Retune bot2's opacity calculation to be brighter when nearby

--- src/bot2.js
+++ src/bot2.js
@@ -123,7 +123,7 @@ export default class Bot2 {
   }
 
   drawCircle(ctx, x, y, width, height, playerDistance) {
-    ctx.globalAlpha = Math.min(1, width/(playerDistance-width/2))
+    ctx.globalAlpha = Math.min(1, width/(playerDistance-width*2))
     ctx.beginPath();
     ctx.arc(x + width/2, y + height/2, 50, 0, 2*Math.PI);
     ctx.stroke();
