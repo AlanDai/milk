64233e70c3f4b349f4b83ce47f87a9908582cd68
alanxudai@gmail.com <alanxudai@gmail.com>
2021-03-31 19:43:53 -0700

    Continue DRYing code - move calcMoveTo function to util

--- src/util.js
+++ src/util.js
@@ -3,6 +3,11 @@ const Util = {
     return Math.sqrt(
       Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)
     )
+  },
+
+  calcMoveTo(speed, sX, sY, dX, dY) {
+    let angle = Math.atan2((dY - sY), (dX - sX));
+    return [speed * Math.cos(angle), speed * Math.sin(angle)];
   }
 }
 
