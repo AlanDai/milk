f5a8be5623be2298fb59da4749e76f43f5171fd0
alanxudai@gmail.com <alanxudai@gmail.com>
2021-04-01 21:13:03 -0700

    Add moveBot method to Bot superclass

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,5 @@
+const Util = require("./util.js")
+
 function Bot (options) {
   this.type = 'bot';
   this.dimensions = options.dimensions;
@@ -5,6 +7,10 @@ function Bot (options) {
   this.x, this.y;
   this.playerDistance;
   this.createdAt = Date.now();
+}
+
+Bot.prototype.moveBot = function(playerX, playerY, speed) {
+  this.playerDistance = Util.dist(playerX, playerY, this.x, this.y);
 }
 
 module.exports = Bot;
