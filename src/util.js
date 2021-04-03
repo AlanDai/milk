557dcb503397b2f07fc74d5787c65418e478270e
alanxudai@gmail.com <alanxudai@gmail.com>
2021-04-02 22:33:02 -0700

    Add inheritance method to util

--- src/util.js
+++ src/util.js
@@ -1,4 +1,11 @@
 const Util = {
+  inherits(childClass, parentClass) {
+    function Surrogate(){};
+    Surrogate.prototype = new Surrogate();
+    childClass.prototype = new Surrogate();
+    childClass.prototype.constructor = childClass;
+  },
+  
   dist(x1, y1, x2, y2) {
     return Math.sqrt(
       Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)
